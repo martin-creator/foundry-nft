14a5b9efa110b9751074333ca76b2bebded431c5
martin-creator <martinlubowa@outlook.com>
2025-02-15 21:03:28 +0300

    Add submodule for `foundry-devops`, update settings for `foundry-devops`, create Makefile with various targets, update `foundry.toml` for permissions, and add SVG images for happy and sad emotions.

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -8,7 +8,7 @@ contract BasicNft is ERC721 {
     uint256 private s_tokenCounter;
     mapping(uint256 => string) private s_tokenIdToUri;
 
-    constructor() ERC721("BasicNFT", "BFT") {
+    constructor() ERC721("Dogie", "DOG") {
         s_tokenCounter = 0;
     }
 
